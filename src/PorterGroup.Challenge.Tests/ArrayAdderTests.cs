18950360491e93cc97148d1cc11ed821d2f09f76
wilgarxia <wilgrxia@gmail.com>
2023-06-19 05:19:28 -0300

    feat: challenge 2 completed

---
+++ src/PorterGroup.Challenge.Tests/ArrayAdderTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+
+namespace PorterGroup.Challenge.Tests;
+
+public class ArrayAdderTests
+{
+    private readonly ArrayAdder _sut = new();
+
+    [Fact]
+    public void Sum_ShouldThrowArgumentException_WhenGivenAnIntegerArrayWithOneMillionItensOrMore()
+    {
+        // Arrange
+        int[] array = new int[1_000_000];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = 1;
+        }
+
+        // Act
+        Action act = () => _sut.Sum(array);
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("The array must have less than 1 million itens.");
+    }
+
+    [Fact]
+    public void Sum_ShouldReturnTheSumOfAllItens_WhenGivenAnIntegerArray()
+    {
+        // Arrange
+        int[] array = new int[999_999];
+        BigInteger expectedSum = 999_999;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = 1;
+        }
+
+        // Act
+        var result = _sut.Sum(array);
+
+        // Assert
+        result.Should().Be(expectedSum);
+    }
+}
---
+++ src/PorterGroup.Challenge/2/ArrayAdder.cs
@@ -0,0 +1,64 @@
+using System.Diagnostics;
+using System.Numerics;
+
+namespace PorterGroup.Challenge;
+
+public class ArrayAdder
+{
+    private readonly object _lock = new();
+
+    public BigInteger Sum(int[] array)
+    {
+        if (array.Length >= 1_000_000)
+            throw new ArgumentException("The array must have less than 1 million itens.");
+
+        ParallelOptions options = new()
+        {
+            MaxDegreeOfParallelism = Process.GetCurrentProcess().Threads.Count
+        };
+
+        long partialSum = 0;
+        BigInteger totalSum = 0;
+
+        try
+        {
+            Parallel.ForEach(array, options, item =>
+            {
+                lock (_lock)
+                {
+                    if (!TryAdd(item, ref partialSum))
+                    {
+                        totalSum += partialSum;
+                        partialSum = 0;
+                        _ = TryAdd(item, ref partialSum);
+                    }
+                }
+            });
+
+            totalSum += partialSum;
+        }
+        catch (OutOfMemoryException ex)
+        {
+            throw new Exception("There's no enough memory to perform the Sum.", ex);
+        }
+
+        return totalSum;
+    }
+
+    private static bool TryAdd(int value, ref long sum)
+    {
+        checked
+        {
+            try
+            {
+                sum += value;
+            }
+            catch (OverflowException)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
